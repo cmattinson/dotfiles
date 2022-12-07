4d546bb9e5d0613d357e1cdce064c7892f214608
Chris Mattinson <cmattinson@method1software.com>
2022-12-07 10:37:26 -0700

    Add windows terminal settings, everforest theming

---
+++ .config/nvim/after/plugin/Base4Tone-nvim.lua
@@ -0,0 +1,1 @@
+vim.cmd("highlight Comment cterm=NONE gui=NONE")
---
+++ .config/nvim/plugin/everforest.lua
@@ -0,0 +1,16 @@
+vim.cmd([[
+        " Important!!
+        if has('termguicolors')
+          set termguicolors
+        endif
+
+        " For dark version.
+        set background=dark
+        " For light version.
+        " Set contrast.
+        " This configuration option should be placed before `colorscheme everforest`.
+        " Available values: 'hard', 'medium'(default), 'soft'
+        let g:everforest_background = 'medium'
+        " For better performance
+        let g:everforest_better_performance = 1
+        colorscheme everforest]])
